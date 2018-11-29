52694bea32fbb755b988b2dbab1fa49680ebc30f
Yiding "Tim" Xie <yx2443@columbia.edu>
2018-11-29 01:21:18 -0500

    Add files via upload
    this function is to remove the lines for those with "" in either tess_err or ground_truth_err column

--- lib/remove_empty_line.R
+++ lib/remove_empty_line.R
@@ -1,3 +1,8 @@
+
+#############################################################################################################
+#### this function is to remove the lines for those with "" in either tess_err or ground_truth_err column ###
+#############################################################################################################
+
 remove_empty_line <- function(current_detect_output){
   empty_index <- which(current_detect_output[,1] == "" |  current_detect_output[,2] == "")
   
